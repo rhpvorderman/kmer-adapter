72cc9c9071c06c736f7d5a47222a78c597641ee5
Ruben Vorderman <r.h.p.vorderman@lumc.nl>
2022-12-16 08:41:22 +0100

    Use quicker kmer heuristic

--- kmer-adapter.py
+++ kmer-adapter.py
@@ -10,13 +10,27 @@ def generate_kmers(sequence: str, k: int):
         yield sequence[i: i+k]
 
 
+def present_kmers(sequence, error_rate):
+    length = len(sequence)
+    max_errors = int(length * error_rate)
+    k = length // (max_errors + 1)
+    return [sequence[i:i+k] for i in  range(0, length, k)][:max_errors + 1]
+
+
 def main():
-    adapter_kmers = set(generate_kmers(sys.argv[2], 8))
+    adapter_kmers = present_kmers(sys.argv[2], 0.1)
+    print(adapter_kmers)
     with dnaio.open(sys.argv[1], mode="r", open_threads=0) as reader:
-        for record in reader:
-            sequence_kmers = set(generate_kmers(record.sequence, 8))
-            if adapter_kmers.isdisjoint(sequence_kmers):
-                pass
+        number_of_records = 0
+        possible_adapters_found = 0
+        for number_of_records, record in enumerate(reader, start=1):
+            sequence = record.sequence
+            for kmer in adapter_kmers:
+                if kmer in sequence:
+                    possible_adapters_found += 1
+                    break
+    print(f"Percentage possible adapters: {possible_adapters_found / number_of_records}")
+
 
 
 if __name__ == "__main__":
