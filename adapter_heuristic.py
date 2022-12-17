7e85ff0e5097bd5f2c8dfcea6c6bcfd76e8571c5
Ruben Vorderman <r.h.p.vorderman@lumc.nl>
2022-12-17 14:04:32 +0100

    Remove unnessary class

--- adapter_heuristic.py
+++ adapter_heuristic.py
@@ -1,71 +1,62 @@
-# from libc.stdint cimport uint16_t
-#
-#
-#
-# ctypedef struct _CheckMer:
-#     char *sequence
-#     Py_ssize_t offset
+from typing import List, Tuple
 
 
-class AdapterHeuristic:
-    # cdef:
-    #     object sequences_obj
-    #     char *sequences
-    #     _CheckMer *checkmers
+def _chunk(sequence: str, chunks: int) -> List[str]:
+    """Chunk a sequence in chunks parts"""
+    chunk_size = len(sequence) // (chunks)
+    remainder = len(sequence) % (chunks)
+    offset = 0
+    chunks = []
+    for _ in range(remainder):
+        chunks.append(sequence[offset:offset + chunk_size + 1])
+        offset += (chunk_size + 1)
+    while offset < len(sequence):
+        chunks.append(sequence[offset:offset + chunk_size])
+        offset += chunk_size
+    return chunks
 
-    @staticmethod
-    def _chunk(sequence, chunks):
-        """Chunk a sequence in chunks parts"""
-        chunk_size = len(sequence) // (chunks)
-        remainder = len(sequence) % (chunks)
-        offset = 0
-        chunks = []
-        for _ in range(remainder):
-            chunks.append(sequence[offset:offset+chunk_size+1])
-            offset += (chunk_size + 1)
-        while offset < len(sequence):
-            chunks.append(sequence[offset:offset+chunk_size])
-            offset += chunk_size
-        return chunks
 
-    def __init__(self, adapter, min_overlap, error_rate):
-        adapter_length = len(adapter)
-        max_errors = int(adapter_length * error_rate)
-        error_lengths = []
-        max_error = 1
-        offsets_and_kmers = []
-        for i in range(adapter_length + 1):
-            if i * error_rate >= max_error:
-                error_lengths.append(i)
-                max_error += 1
+def create_kmers_and_offsets(adapter: str, min_overlap: int, error_rate: float
+                             ) -> List[Tuple[str, int]]:
+    adapter_length = len(adapter)
+    max_errors = int(adapter_length * error_rate)
+    error_lengths = []
+    max_error = 1
+    kmers_and_offsets = []
+    for i in range(adapter_length + 1):
+        if i * error_rate >= max_error:
+            error_lengths.append(i)
+            max_error += 1
 
-        # Build up the array with chunks which should occur at the tail end
-        # if the adapter overlaps with the end.
-        min_overlap_kmer = adapter[:min_overlap]
-        min_overlap_kmer_offset = -(error_lengths[0] -1)
-        offsets_and_kmers.append((min_overlap_kmer_offset, min_overlap_kmer))
-        for i, error_length in enumerate(error_lengths):
-            if (i + 1) < len(error_lengths):
-                next_length = error_lengths[i + 1]
-            else:
-                next_length = adapter_length
-            offset = -(next_length - 1)
-            number_of_errors = i + 1
-            chunks = self._chunk(adapter[:error_length], number_of_errors + 1)
-            chunk_offset = offset
-            for chunk in chunks:
-                offsets_and_kmers.append((chunk_offset, chunk))
-
-        # Create kmers at least one of which should be in the read when there is a
-        # an adapter
-        chunks = self._chunk(adapter, max_errors + 1)
-        chunk_offset = 0
+    # Build up the array with chunks which should occur at the tail end
+    # if the adapter overlaps with the end.
+    min_overlap_kmer = adapter[:min_overlap]
+    min_overlap_kmer_offset = -(error_lengths[0] - 1)
+    kmers_and_offsets.append((min_overlap_kmer, min_overlap_kmer_offset))
+    for i, error_length in enumerate(error_lengths):
+        if (i + 1) < len(error_lengths):
+            next_length = error_lengths[i + 1]
+        else:
+            next_length = adapter_length
+        offset = -(next_length - 1)
+        number_of_errors = i + 1
+        chunks = _chunk(adapter[:error_length], number_of_errors + 1)
+        chunk_offset = offset
         for chunk in chunks:
-            offsets_and_kmers.append((chunk_offset, chunk))
-        self.offsets_and_kmers = offsets_and_kmers
+            kmers_and_offsets.append((chunk, chunk_offset))
 
-    def adapter_possibly_present(self, sequence):
-        for offset, kmer in self.offsets_and_kmers:
-            if sequence.find(kmer, offset) != -1:
-                return True
-        return False
+    # Create kmers at least one of which should be in the read when there is a
+    # an adapter
+    chunks = _chunk(adapter, max_errors + 1)
+    chunk_offset = 0
+    for chunk in chunks:
+        kmers_and_offsets.append((chunk, chunk_offset))
+    return kmers_and_offsets
+
+
+def kmers_present_in_sequence(kmers_and_offsets: List[Tuple[str, int]],
+                              sequence: str):
+    for kmer, offset in kmers_and_offsets:
+        if sequence.find(kmer, offset) != -1:
+            return True
+    return False
--- kmer-adapter.py
+++ kmer-adapter.py
@@ -5,7 +5,7 @@ import argparse
 import dnaio
 
 
-from adapter_heuristic import AdapterHeuristic
+from adapter_heuristic import create_kmers_and_offsets, kmers_present_in_sequence
 
 def illumina_truseq_candidate(sequence):
     """
@@ -67,8 +67,8 @@ def main():
     parser.add_argument("with_adapter")
     parser.add_argument("no_adapter")
     args = parser.parse_args()
-    adapter_heuristic = AdapterHeuristic(args.adapter, 3, 0.1)
-    print(adapter_heuristic.offsets_and_kmers)
+    kmers_and_offsets = create_kmers_and_offsets(args.adapter, 3, 0.1)
+    print(kmers_and_offsets)
     with (
         dnaio.open(args.fastq, mode="r", open_threads=0) as reader,
         open(args.with_adapter, mode="wb") as with_adapter,
@@ -77,7 +77,7 @@ def main():
         number_of_records = 0
         possible_adapters_found = 0
         for number_of_records, record in enumerate(reader, start=1):
-            if adapter_heuristic.adapter_possibly_present(record.sequence):
+            if kmers_present_in_sequence(kmers_and_offsets, record.sequence):
                 with_adapter.write(record.fastq_bytes())
                 possible_adapters_found += 1
             else:
