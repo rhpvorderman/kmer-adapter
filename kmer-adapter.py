2083a6e2043aadc93e50f5a676335b47ffcd794f
Ruben Vorderman <r.h.p.vorderman@lumc.nl>
2022-12-28 07:20:08 +0100

    Add cutadapt code

--- kmer-adapter.py
+++ kmer-adapter.py
@@ -3,7 +3,7 @@
 import argparse
 import itertools
 import sys
-from typing import List, Set, Tuple
+from typing import List, Optional, Set, Tuple
 from collections import defaultdict
 
 import dnaio
@@ -24,61 +24,89 @@ def kmer_possibilities(sequence: str, chunks: int) -> List[Set[str]]:
     """
     chunk_size = len(sequence) // (chunks)
     remainder = len(sequence) % (chunks)
-    chunk_sizes: List[int] = remainder * [chunk_size + 1] + (chunks - remainder) * [chunk_size]
+    chunk_sizes: List[int] = remainder * [chunk_size + 1] + (chunks - remainder) * [
+        chunk_size
+    ]
     possible_orderings = set(itertools.permutations(chunk_sizes))
     kmer_sets = []
     for chunk_list in possible_orderings:
         offset = 0
         chunk_set = set()
         for size in chunk_list:
-            chunk_set.add(sequence[offset:offset + size])
+            chunk_set.add(sequence[offset : offset + size])
             offset += size
         kmer_sets.append(chunk_set)
     return kmer_sets
 
 
-# A SearchSet is an offset combined with a list of possible kmer sets which
-# should appear after this offset
-SearchSet = Tuple[int, List[Set[str]]]
+# A SearchSet is a start and stop combined with a list of possible kmer sets
+# which should appear between this start and stop. Start and stop follow python
+# indexing rules. (Negative start is a position relative to the end. None end
+# is to the end of the sequence)
+SearchSet = Tuple[int, Optional[int], List[Set[str]]]
 
 
-def find_optimal_kmers(search_sets: List[SearchSet]) -> List[Tuple[str, int]]:
-    minimal_score = sys.maxsize
-    best_combination = None
-    offsets = [offset for offset, kmer_set_list in search_sets]
-    kmer_set_lists = [kmer_set_list for offset, kmer_set_list in search_sets]
-    for kmer_sets in itertools.product(*kmer_set_lists):
-        check_set = set()
-        for s in kmer_sets:
-            check_set |= s
-        if len(check_set) < minimal_score:
-            best_combination = kmer_sets
-            minimal_score = len(check_set)
+def minimize_kmer_search_list(
+    kmer_search_list: List[Tuple[str, int, Optional[int]]]
+) -> List[Tuple[str, int, Optional[int]]]:
     kmer_and_offsets_dict = defaultdict(list)
-    for offset, kmer_set in zip(offsets, best_combination):
-        for kmer in kmer_set:
-            kmer_and_offsets_dict[kmer].append(offset)
-    kmers_and_offsets: List[Tuple[str, int]] = []
-    for kmer, offsets in kmer_and_offsets_dict.items():
-        if len(offsets) == 1:
-            offset = offsets[0]
-        elif 0 in offsets:
-            offset = 0
-        # If all offsets have the same sign then choose the offset closest to
-        # the start.
-        elif all(offset < 0 for offset in offsets) or \
-                all(offset > 0 for offset in offsets):
-            offset = min(offsets)
-        else: # Mixed positive and negative: search the entire sequence
-            offset = 0
-        kmers_and_offsets.append((kmer, offset))
-    return kmers_and_offsets
+    for kmer, start, stop in kmer_search_list:  # type: ignore
+        kmer_and_offsets_dict[kmer].append((start, stop))
+    kmers_and_positions: List[Tuple[str, int, Optional[int]]] = []
+    for kmer, positions in kmer_and_offsets_dict.items():
+        if len(positions) == 1:
+            start, stop = positions[0]
+            kmers_and_positions.append((kmer, start, stop))
+            continue
+        if (0, None) in positions:
+            kmers_and_positions.append((kmer, 0, None))
+            continue
+        front_searches = [(start, stop) for start, stop in positions if start == 0]
+        back_searches = [(start, stop) for start, stop in positions if stop is None]
+        middle_searches = [
+            (start, stop)
+            for start, stop in positions
+            if start != 0 and stop is not None
+        ]
+        if middle_searches:
+            raise NotImplementedError(
+                "Situations with searches starting in the middle have not been considered."
+            )
+        if front_searches:
+            # (0, None) condition is already catched, so stop is never None.
+            kmers_and_positions.append(
+                (kmer, 0, max(stop for start, stop in front_searches))  # type: ignore
+            )
+        if back_searches:
+            kmers_and_positions.append(
+                (kmer, min(start for start, stop in back_searches), None)
+            )
+    return kmers_and_positions
 
 
-def create_kmers_and_offsets(adapter: str, min_overlap: int, error_rate: float
-                             ) -> List[Tuple[str, int]]:
+def find_optimal_kmers(
+    search_sets: List[SearchSet],
+) -> List[Tuple[str, int, Optional[int]]]:
+    minimal_score = sys.maxsize
+    best_combination = []
+    positions = [(start, stop) for start, stop, kmer_set_list in search_sets]
+    kmer_set_lists = [kmer_set_list for start, stop, kmer_set_list in search_sets]
+    for kmer_sets in itertools.product(*kmer_set_lists):
+        kmer_search_list = []
+        for kmer_set, (start, stop) in zip(kmer_sets, positions):
+            for kmer in kmer_set:
+                kmer_search_list.append((kmer, start, stop))
+        minimized_search_list = minimize_kmer_search_list(kmer_search_list)
+        if len(minimized_search_list) < minimal_score:
+            best_combination = minimized_search_list
+            minimal_score = len(minimized_search_list)
+    return best_combination
+
+
+def create_back_overlap_searchsets(
+    adapter: str, min_overlap: int, error_rate: float
+) -> List[SearchSet]:
     adapter_length = len(adapter)
-    max_errors = int(adapter_length * error_rate)
     error_lengths = []
     max_error = 1
     search_sets: List[SearchSet] = []
@@ -93,51 +121,102 @@ def create_kmers_and_offsets(adapter: str, min_overlap: int, error_rate: float
     min_overlap_kmer_length = 5
     if min_overlap < min_overlap_kmer_length:
         for i in range(min_overlap, min_overlap_kmer_length):
-            search_sets.append((-i, [{adapter[:i],}]))
+            search_sets.append(
+                (
+                    -i,
+                    None,
+                    [
+                        {
+                            adapter[:i],
+                        }
+                    ],
+                )
+            )
         min_overlap = min_overlap_kmer_length
     # Build up the array with chunks which should occur at the tail end
     # if the adapter overlaps with the end.
     min_overlap_kmer = adapter[:min_overlap]
-    min_overlap_kmer_offset = -(error_lengths[0] - 1) if error_lengths else -(adapter_length - 1)
-    search_sets.append((min_overlap_kmer_offset, [{min_overlap_kmer,}]))
+    min_overlap_kmer_start = (
+        -(error_lengths[0] - 1) if error_lengths else -(adapter_length - 1)
+    )
+    search_sets.append(
+        (
+            min_overlap_kmer_start,
+            None,
+            [
+                {
+                    min_overlap_kmer,
+                }
+            ],
+        )
+    )
     for i, error_length in enumerate(error_lengths):
         if (i + 1) < len(error_lengths):
             next_length = error_lengths[i + 1]
         else:
             next_length = adapter_length
-        offset = -(next_length - 1)
+        start = -(next_length - 1)
         number_of_errors = i + 1
         kmer_sets = kmer_possibilities(adapter[:error_length], number_of_errors + 1)
-        search_sets.append((offset, kmer_sets))
+        search_sets.append((start, None, kmer_sets))
+    return search_sets
 
-    # Create kmers at least one of which should be in the read when there is a
-    # an adapter
+
+def create_kmers_and_offsets(
+    adapter: str,
+    min_overlap: int,
+    error_rate: float,
+    back_adapter: bool,
+    front_adapter: bool,
+) -> List[Tuple[str, int, Optional[int]]]:
+    max_errors = int(len(adapter) * error_rate)
+    search_sets = []
+    if back_adapter:
+        search_sets.extend(
+            create_back_overlap_searchsets(adapter, min_overlap, error_rate)
+        )
+    if front_adapter:
+        # To create a front adapter the code is practically the same except
+        # with some parameters set differently. Reversing the adapter, running
+        # the back adapter code and reversing all the kmers and positions has
+        # the same effect without needing to duplicate the code.
+        reversed_back_search_sets = create_back_overlap_searchsets(
+            adapter[::-1], min_overlap, error_rate
+        )
+        front_search_sets = []
+        for start, stop, kmer_sets in reversed_back_search_sets:
+            new_kmer_sets = [
+                {kmer[::-1] for kmer in kmer_set} for kmer_set in kmer_sets
+            ]
+            front_search_sets.append((0, -start, new_kmer_sets))
+        search_sets.extend(front_search_sets)
     kmer_sets = kmer_possibilities(adapter, max_errors + 1)
-    search_sets.append((0, kmer_sets))
+    search_sets.append((0, None, kmer_sets))
     return find_optimal_kmers(search_sets)
 
 
-def kmers_present_in_sequence(kmers_and_offsets: List[Tuple[str, int]],
-                              sequence: str):
-    for kmer, offset in kmers_and_offsets:
-        if sequence.find(kmer, offset) != -1:
-            return True
-    return False
-
-
-def kmer_probability_analysis(kmers_and_offsets: List[Tuple[str , int]],
-                              default_length: int = 150):
-    print("kmer\toffset\tconsidered sites\thit chance by random sequence (%)")
+def kmer_probability_analysis(
+    kmers_and_offsets: List[Tuple[str, int, Optional[int]]], default_length: int = 150
+):
+    print("kmer\tstart\tstop\tconsidered sites\thit chance by random sequence (%)")
     accumulated_not_hit_chance = 1.0
-    for kmer, offset in kmers_and_offsets:
+    for kmer, start, stop in kmers_and_offsets:
         kmer_length = len(kmer)
-        check_length = -offset if offset < 0 else default_length - offset
+        if stop is None:
+            check_length = -start if start < 0 else default_length - start
+        else:
+            start = default_length - start if start < 0 else start
+            check_length = max(stop - start, 0)
         considered_sites = check_length - kmer_length + 1
-        single_kmer_hit_chance = 1 / 4 ** kmer_length
+        single_kmer_hit_chance = 1 / 4**kmer_length
         not_hit_chance = (1 - single_kmer_hit_chance) ** considered_sites
         accumulated_not_hit_chance *= not_hit_chance
-        print(f"{kmer:10}\t{offset}\t{considered_sites}\t{(1 - not_hit_chance) * 100:.2f}")
-    print(f"Chance for profile hit by random sequence: {(1 - accumulated_not_hit_chance) * 100:.2f}%")
+        print(
+            f"{kmer:10}\t{start}\t{stop}\t{considered_sites}\t{(1 - not_hit_chance) * 100:.2f}"
+        )
+    print(
+        f"Chance for profile hit by random sequence: {(1 - accumulated_not_hit_chance) * 100:.2f}%"
+    )
 
 
 def main():
