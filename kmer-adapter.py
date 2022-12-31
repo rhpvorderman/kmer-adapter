86fcc68f458d8465369f73b13b2b9a1f6a4bc1d5
Ruben Vorderman <r.h.p.vorderman@lumc.nl>
2022-12-31 08:12:33 +0100

    Do not write to optimally benchmark

--- kmer-adapter.py
+++ kmer-adapter.py
@@ -231,8 +231,6 @@ def main():
     parser.add_argument("--adapter")
     parser.add_argument("--anywhere", action="store_true")
     parser.add_argument("fastq")
-    parser.add_argument("with_adapter")
-    parser.add_argument("no_adapter")
     args = parser.parse_args()
     kmers_and_offsets = create_kmers_and_positions(
         args.adapter, 3, 0.1, back_adapter=True, front_adapter=args.anywhere)
@@ -240,17 +238,12 @@ def main():
     print(kmer_probability_analysis(kmers_and_offsets))
     with (
         dnaio.open(args.fastq, mode="r", open_threads=0) as reader,
-        open(args.with_adapter, mode="wb") as with_adapter,
-        open(args.no_adapter, mode="wb") as no_adapter,
     ):
         number_of_records = 0
         possible_adapters_found = 0
         for number_of_records, record in enumerate(reader, start=1):
             if kmer_finder.kmers_present(record.sequence):
-                with_adapter.write(record.fastq_bytes())
                 possible_adapters_found += 1
-            else:
-                no_adapter.write(record.fastq_bytes())
     print(f"Percentage possible adapters: {possible_adapters_found / number_of_records}")
 
 
