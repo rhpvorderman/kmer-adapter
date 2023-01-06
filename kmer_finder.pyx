71bdade9f448b73f3a7539923ac4f373a9dbc60b
Ruben Vorderman <r.h.p.vorderman@lumc.nl>
2023-01-06 06:57:13 +0100

    Create multimask structures

--- kmer_finder.pyx
+++ kmer_finder.pyx
@@ -1,7 +1,7 @@
 # cython: profile=False, emit_code_comments=False, language_level=3
 
 from cpython.mem cimport PyMem_Malloc, PyMem_Free
-from libc.string cimport memset, strlen
+from libc.string cimport memcpy, memset, strlen
 
 from cpython.unicode cimport PyUnicode_CheckExact, PyUnicode_GET_LENGTH
 from libc.stdint cimport uint8_t
@@ -17,11 +17,12 @@ cdef extern from "Python.h":
     void *PyUnicode_DATA(object o)
     bint PyUnicode_IS_COMPACT_ASCII(object o)
 
-ctypedef struct KmerEntry:
-    size_t kmer_length
+ctypedef struct KmerSearchEntry:
     size_t mask_offset
     ssize_t search_start
     ssize_t search_stop
+    bitmask_t zero_mask
+    bitmask_t found_mask
 
 
 cdef class KmerFinder:
@@ -43,66 +44,80 @@ cdef class KmerFinder:
     This has a lot of python overhead. The following code accomplishes the
     same task and allows for case-independent matching:
 
-        kmers_and_positions = [("AGA", -10, None), ("AGCATGA", 0, None)]
-        kmer_finder = KmerFinder(kmers_and_positions)
+        positions_and_kmers = [(-10, None, ["AGA"]), (0, None, ["AGCATGA"])]
+        kmer_finder = KmerFinder(positions_and_kmers)
         for sequence in sequences:
             if kmer_finder.kmers_present(sequence):
                 # do something
-                pass
+                continue
 
     This is more efficient as the kmers_present method can be applied to a lot
     of sequences and all the necessary unpacking for each kmer into C variables
     happens only once.
     """
     cdef:
-        KmerEntry *kmer_entries
-        bitmask_t *kmer_masks
-        size_t number_of_kmers
-        readonly object kmers_and_positions
+        KmerSearchEntry *search_entries
+        bitmask_t *search_masks
+        size_t number_of_searches
+        readonly object positions_and_kmers
         readonly bint ref_wildcards
         readonly bint query_wildcards
 
-    def __cinit__(self, kmers_and_positions, ref_wildcards=False, query_wildcards=False):
-        self.kmer_masks = NULL
-        self.kmer_entries = NULL
-        self.number_of_kmers = 0
+    def __cinit__(self, positions_and_kmers, ref_wildcards=False, query_wildcards=False):
+        cdef char[64] search_word
+        self.search_masks = NULL
+        self.search_entries = NULL
+        self.number_of_searches = 0
         self.ref_wildcards = ref_wildcards
         self.query_wildcards = query_wildcards
-        number_of_entries = len(kmers_and_positions)
-        self.kmer_entries = <KmerEntry *>PyMem_Malloc(number_of_entries * sizeof(KmerEntry))
-        # for the kmers the NULL bytes also need space.
-        self.kmer_masks = <bitmask_t *>PyMem_Malloc(number_of_entries * sizeof(bitmask_t) * BITMASK_INDEX_SIZE)
-        self.number_of_kmers = number_of_entries
+        number_of_entries = len(positions_and_kmers)
+        self.search_entries = <KmerSearchEntry *>PyMem_Malloc(number_of_entries * sizeof(KmerSearchEntry))
+        self.search_masks = <bitmask_t *>PyMem_Malloc(number_of_entries * sizeof(bitmask_t) * BITMASK_INDEX_SIZE)
+        self.number_of_searches = number_of_entries
         cdef size_t mask_offset = 0
         cdef char *kmer_ptr
+        cdef size_t offset
+        cdef bitmask_t zero_mask, found_mask
         cdef Py_ssize_t kmer_length
-        for i, (kmer, start, stop) in enumerate(kmers_and_positions):
-            if not PyUnicode_CheckExact(kmer):
-                raise TypeError(f"Kmer should be a string not {type(kmer)}")
-            if not PyUnicode_IS_COMPACT_ASCII(kmer):
-                raise ValueError("Only ASCII strings are supported")
-            self.kmer_entries[i].search_start  = start
+        for i, (start, stop, kmers) in enumerate(positions_and_kmers):
+            memset(search_word, 0, 64)
+            offset = 0
+            zero_mask = ~0
+            found_mask = 0
+            for kmer in kmers:
+                if not PyUnicode_CheckExact(kmer):
+                    raise TypeError(f"Kmer should be a string not {type(kmer)}")
+                if not PyUnicode_IS_COMPACT_ASCII(kmer):
+                    raise ValueError("Only ASCII strings are supported")
+                zero_mask ^= <bitmask_t>1ULL << offset
+                kmer_length = PyUnicode_GET_LENGTH(kmer)
+                kmer_ptr = <char *> PyUnicode_DATA(kmer)
+                memcpy(search_word, kmer_ptr, kmer_length)
+                search_word[kmer_length] = 0
+                found_mask |= <bitmask_t>1ULL << (offset + kmer_length)
+                offset = offset + kmer_length + 1
+            self.search_entries[i].search_start  = start
             if stop is None:
                 stop = 0
-            self.kmer_entries[i].search_stop = stop
-            self.kmer_entries[i].mask_offset = mask_offset
-            kmer_length = PyUnicode_GET_LENGTH(kmer)
-            self.kmer_entries[i].kmer_length = kmer_length
-            kmer_ptr = <char *>PyUnicode_DATA(kmer)
-            populate_needle_mask(self.kmer_masks + mask_offset, kmer_ptr, kmer_length,
+            self.search_entries[i].search_stop = stop
+            self.search_entries[i].mask_offset = mask_offset
+            self.search_entries[i].zero_mask = zero_mask
+            self.search_entries[i].found_mask = found_mask
+            populate_needle_mask(self.search_masks + mask_offset, search_word, offset,
                                  self.ref_wildcards, self.query_wildcards)
             mask_offset += BITMASK_INDEX_SIZE
-        self.kmers_and_positions = kmers_and_positions
+        self.positions_and_kmers = positions_and_kmers
 
     def __reduce__(self):
-        return KmerFinder, (self.kmers_and_positions, self.ref_wildcards, self.query_wildcards)
+        return KmerFinder, (self.positions_and_kmers, self.ref_wildcards, self.query_wildcards)
 
     def kmers_present(self, str sequence):
         cdef:
-            KmerEntry entry
+            KmerSearchEntry entry
             size_t i
             size_t kmer_offset
-            size_t kmer_length
+            bitmask_t zero_mask
+            bitmask_t found_mask
             ssize_t start, stop
             const bitmask_t *mask_ptr
             const char *search_ptr
@@ -112,8 +127,8 @@ cdef class KmerFinder:
             raise ValueError("Only ASCII strings are supported")
         cdef const char *seq = <char *>PyUnicode_DATA(sequence)
         cdef Py_ssize_t seq_length = PyUnicode_GET_LENGTH(sequence)
-        for i in range(self.number_of_kmers):
-            entry = self.kmer_entries[i]
+        for i in range(self.number_of_searches):
+            entry = self.search_entries[i]
             start = entry.search_start 
             stop = entry.search_stop
             if start < 0:
@@ -132,17 +147,18 @@ cdef class KmerFinder:
             if search_length <= 0:
                 continue
             search_ptr = seq + start
-            kmer_length = entry.kmer_length
-            mask_ptr = self.kmer_masks + entry.mask_offset
+            zero_mask = entry.zero_mask
+            found_mask = entry.found_mask
+            mask_ptr = self.search_masks + entry.mask_offset
             search_result = shift_or_multiple_is_present(
-                search_ptr, search_length, mask_ptr, <bitmask_t>~1, <bitmask_t>1ULL<<kmer_length)
+                search_ptr, search_length, mask_ptr, zero_mask, found_mask)
             if search_result:
                 return True
         return False
 
     def __dealloc__(self):
-        PyMem_Free(self.kmer_masks)
-        PyMem_Free(self.kmer_entries)
+        PyMem_Free(self.search_masks)
+        PyMem_Free(self.search_entries)
 
 
 cdef void set_masks(bitmask_t *needle_mask, size_t pos, const char *chars):
@@ -161,6 +177,8 @@ cdef populate_needle_mask(bitmask_t *needle_mask, const char *needle, size_t nee
     memset(needle_mask, 0xff, sizeof(bitmask_t) * BITMASK_INDEX_SIZE)
     for i in range(needle_length):
         c = needle[i]
+        if c == 0:
+            continue
         if c == b"A" or c == b"a":
             set_masks(needle_mask, i, "Aa")
             if query_wildcards:
